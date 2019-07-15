ef58095b87c6b51df3920d2e0759744a60248a4a
Bruno d'Auria <bdauria@centreon.com>
2019-07-15 09:29:29 +0200

    Remove solution implementations

--- src/exo1/index.test.js
+++ src/exo1/index.test.js
@@ -1,17 +1,5 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Exo1 from ".";
+import Exo2 from ".";
 
-describe("Exo1", () => {
-  it("renders with loading indicator when loading is true", () => {
-    const { container } = render(<Exo1 loading />);
-
-    expect(container.firstChild).toMatchSnapshot();
-  });
-
-  it("renders nothing when loading is false", () => {
-    const { container } = render(<Exo1 />);
-
-    expect(container.firstChild).toMatchSnapshot();
-  });
-});
+describe("Exo2", () => {});
--- src/exo3/index.test.js
+++ src/exo3/index.test.js
@@ -1,30 +1,5 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Exo2 from ".";
-import axios from "axios";
 
-jest.mock("axios");
-
-describe("Exo2", () => {
-  const retrievedUsers = [{ name: "Bob" }, { name: "Alice " }];
-
-  beforeEach(() => {
-    axios.get.mockImplementation(() =>
-      Promise.resolve({ data: retrievedUsers })
-    );
-  });
-
-  it("renders the names of retrieved users", async () => {
-    const { getByText, findByText } = render(<Exo2 />);
-    await findByText(/Total users/);
-    expect(getByText(/Bob/)).toBeInTheDocument();
-    expect(getByText(/Alice/)).toBeInTheDocument();
-  });
-
-  it("renders the total number of users", async () => {
-    const { getByText, findByText } = render(<Exo2 />);
-    await findByText(/Total users/);
-
-    expect(getByText(/Total users: 2/)).toBeInTheDocument();
-  });
-});
+describe("Exo2", () => {});

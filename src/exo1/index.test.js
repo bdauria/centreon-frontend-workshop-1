310def6d7f705d4dad2afdad43603382ea1842bb
Bruno d'Auria <bdauria@centreon.com>
2019-07-15 09:35:54 +0200

    Implement Exo1

--- src/exo1/index.test.js
+++ src/exo1/index.test.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Exo2 from ".";
+import Exo1 from ".";
 
-describe("Exo2", () => {});
+describe("Exo1", () => {
+  it("renders with loading indicator when loading is true", () => {
+    const { container } = render(<Exo1 loading />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders nothing when loading is false", () => {
+    const { container } = render(<Exo1 />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});

07668a528f3a33a39391284f92f71b72a7663024
Bruno d'Auria <bdauria@centreon.com>
2019-07-15 09:47:51 +0200

    Implement solution to Exo3

--- src/exo3/index.test.js
+++ src/exo3/index.test.js
@@ -1,5 +1,30 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Exo2 from ".";
+import Exo3 from ".";
+import axios from "axios";
 
-describe("Exo2", () => {});
+jest.mock("axios");
+
+describe("Exo3", () => {
+  const retrievedUsers = [{ name: "Bob" }, { name: "Alice " }];
+
+  beforeEach(() => {
+    axios.get.mockImplementation(() =>
+      Promise.resolve({ data: retrievedUsers })
+    );
+  });
+
+  it("renders the names of retrieved users", async () => {
+    const { getByText, findByText } = render(<Exo2 />);
+    await findByText(/Total users/);
+    expect(getByText(/Bob/)).toBeInTheDocument();
+    expect(getByText(/Alice/)).toBeInTheDocument();
+  });
+
+  it("renders the total number of users", async () => {
+    const { getByText, findByText } = render(<Exo2 />);
+    await findByText(/Total users/);
+
+    expect(getByText(/Total users: 2/)).toBeInTheDocument();
+  });
+});
